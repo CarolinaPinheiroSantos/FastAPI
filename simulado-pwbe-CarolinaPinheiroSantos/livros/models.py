70ac920ee6ca6377a54847b2e5a6393cf1d6ff21
Carolina Pinheiro <pinheirocarol0506@gmail.com>
2025-04-03 17:02:16 -0300

    bla

---
+++ simulado-pwbe-CarolinaPinheiroSantos/livros/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Livros(models.Model):
+    titulo = models.CharField(max_length=50)
+    autor = models.CharField(max_length=30)
+    paginas = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.titulo
