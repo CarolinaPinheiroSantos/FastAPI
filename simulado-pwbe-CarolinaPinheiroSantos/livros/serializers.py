1ac88db0d252b308d950cd734300fb7289839126
Carolina Pinheiro <pinheirocarol0506@gmail.com>
2025-04-03 22:08:56 -0300

    Delete simulado-pwbe-CarolinaPinheiroSantos directory

--- simulado-pwbe-CarolinaPinheiroSantos/livros/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from . models import Livros
-
-class LivrosSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Livros
-        fields = '__all__'
-      
